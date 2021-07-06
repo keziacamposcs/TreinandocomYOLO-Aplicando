0395e7e99158bb357ff3ac7c104463f50fdd484f
Kezia Campos <32270979+keziacamposcs@users.noreply.github.com>
2021-07-06 10:34:56 -0300

    Create webcam.py

---
+++ webcam.py
@@ -0,0 +1,71 @@
+import cv2
+import numpy as np
+
+#Baixe depois do treinamento o 'weights' que voce vai querer usar nesse programa e coloca abaixo nos conformes:
+def load_yolo():
+	net = cv2.dnn.readNet("yolov4.weights", "yolov4-custom.cfg")
+	classes = []
+	with open("coco.names", "r") as f:
+		classes = [line.strip() for line in f.readlines()]
+	layers_names = net.getLayerNames()
+	output_layers = [layers_names[i[0]-1] for i in net.getUnconnectedOutLayers()]
+	colors = np.random.uniform(0, 255, size=(len(classes), 3))
+	return net, classes, colors, output_layers
+
+
+def detect_objects(img, net, outputLayers):			
+	blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+	net.setInput(blob)
+	outputs = net.forward(outputLayers)
+	return blob, outputs
+
+def get_box_dimensions(outputs, height, width):
+	boxes = []
+	confs = []
+	class_ids = []
+	for output in outputs:
+		for detect in output:
+			scores = detect[5:]
+			print(scores)
+			class_id = np.argmax(scores)
+			conf = scores[class_id]
+			if conf > 0.3:
+				center_x = int(detect[0] * width)
+				center_y = int(detect[1] * height)
+				w = int(detect[2] * width)
+				h = int(detect[3] * height)
+				x = int(center_x - w/2)
+				y = int(center_y - h / 2)
+				boxes.append([x, y, w, h])
+				confs.append(float(conf))
+				class_ids.append(class_id)
+	return boxes, confs, class_ids
+
+#Webcam
+def webcam_detect():
+	model, classes, colors, output_layers = load_yolo()
+	cap = start_webcam()
+	while True:
+		_, frame = cap.read()
+		height, width, channels = frame.shape
+		blob, outputs = detect_objects(frame, model, output_layers)
+		boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+		draw_labels(boxes, confs, colors, class_ids, classes, frame)
+		key = cv2.waitKey(1)
+		if key == 27:
+			break
+	cap.release()
+
+def start_video(video_path):
+	model, classes, colors, output_layers = load_yolo()
+	cap = cv2.VideoCapture(video_path)
+	while True:
+		_, frame = cap.read()
+		height, width, channels = frame.shape
+		blob, outputs = detect_objects(frame, model, output_layers)
+		boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+		draw_labels(boxes, confs, colors, class_ids, classes, frame)
+		key = cv2.waitKey(1)
+		if key == 27:
+			break
+	cap.release()
