2e1887b514709335bc5eb9d5a537b9d98dc228fe
Kezia Campos <32270979+keziacamposcs@users.noreply.github.com>
2021-07-08 10:19:10 -0300

    Update webcam.py

--- webcam.py
+++ webcam.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
 import time
-# Load Yolo
+
+
+#Carregando YOLOv4: seus pesos treinados e sua configuração
 net = cv2.dnn.readNet("yolov4.weights", "yolov4-custom.cfg")
 classes = []
+#obj.names onde se encontra o nome dos objetos
 with open("obj.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
 layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 colors = np.random.uniform(0, 255, size=(len(classes), 2))
 
-# Loading camera
+# Carregando a camera: atente-se o valor do VideoCapture
 cap = cv2.VideoCapture(0)
 font = cv2.FONT_HERSHEY_PLAIN
 starting_time = time.time()
@@ -21,11 +24,11 @@ while True:
     frame_id += 1
     height, width, channels = frame.shape
 
-     # Detecting objects
+     # Detectando os objetos
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
-    # Showing informations on the screen
+    # Mostrando as informações na tela
     class_ids = []
     confidences = []
     boxes = []
@@ -35,12 +38,12 @@ while True:
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.2:
-                # Object detected
+                # Apresenta os objetos detectados nos quadrados
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
-                # Rectangle coordinates
+                # Coordenadas do retangulo
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
                 boxes.append([x, y, w, h])
